e2996c4ab56742a03fdc25a95c6a21bc65b939f8
axell-brendow <breno.axel@gmail.com>
2021-05-01 14:37:59 -0300

    Add JPA main file

---
+++ working-with-jpa/src/main/java/org/example/Main.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+//        try (var part1 = new Part1()) {
+//            part1.run();
+//        }
+//        try (var part2 = new Part2()) {
+//            part2.run();
+//        }
+        try (var part3 = new Part3()) {
+            part3.run();
+        }
+    }
+}
