aaadb39b860ad89acb6c5ef3f5d70e86100eb399
axell-brendow <breno.axel@gmail.com>
2021-05-08 08:30:55 -0300

    Add project that uses gradle as build system

---
+++ java-ambiente/exemplo-gradle/src/test/java/br/com/axellbrendow/exemplogradle/ExemploGradleApplicationTests.java
@@ -0,0 +1,15 @@
+package br.com.axellbrendow.exemplogradle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExemploGradleApplicationTests {
+
+    @SuppressWarnings("PMD")
+    @Test
+    void contextLoads() {
+        assert true;
+    }
+
+}
